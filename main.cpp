4eed8f598b176937c08005f81a83f2b469df7ffa
MiniCosta <57851306+MiniCosta@users.noreply.github.com>
2021-04-16 16:34:47 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include "putvoxel.h"
+#include "cutvoxel.h"
+#include "putbox.h"
+#include "cutbox.h"
+#include "putsphere.h"
+#include "cutsphere.h"
+#include "putellipsoid.h"
+#include "cutellipsoid.h"
+#include "intepretador.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    Sculptor *s1;
+
+    Interpretador parser;
+    std:: vector<FiguraGeometrica*> figs;
+
+    figs = parser.parse("ler.txt");
+
+    s1 = new Sculptor(parser.getDimx(), parser.getDimy(), parser.getDimz());
+cout<< figs.size() <<endl;
+    for(size_t i=0; i<figs.size(); i++){
+                figs[i]->draw(*s1);
+    }
+    s1->limpaVoxels();
+    s1->writeOFF((char*)"saida.OFF");
+    for(size_t i=0; i<figs.size(); i++){
+        delete figs[i];
+    }
+    delete s1;
+    cout<< "Deu certo" <<endl;
+    return 0;
+}
